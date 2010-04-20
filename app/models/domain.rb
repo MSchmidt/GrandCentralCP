fab2d79283bfef246d452636b0d0e28993a9b97b
Matthias Schmidt <mail@wolax.de>
2010-04-20 15:08:29 +0200

    added first chuncks for apache config

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,2 +1,29 @@
 class Domain < ActiveRecord::Base
+  def save_with_config
+    #save
+    begin
+      servername = "mysite.com"
+      serveralias = "www." + servername
+      
+      vhost_template = read_vhost_template
+      
+      vhost_template.gsub! '%servername%', servername
+      vhost_template.gsub! '%serveralias%', serveralias
+      puts vhost_template
+      
+      target_dir = "/Applications/XAMPP/htdocs/ror/GrandCentralCP_testenv/apache2/sites-available"
+      Dir.chdir(target_dir)
+      puts Dir.pwd
+      File.open("domain.txt", "w") do |f|
+        f.write(vhost_template)
+      end
+    rescue Errno::ENOENT
+      puts "No such directory"
+    end
+  end
+  
+  def read_vhost_template
+    template_file = RAILS_ROOT + '/app/templates/apache2_vhost.conf'
+    IO.read(template_file)
+  end
 end
