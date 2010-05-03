c6f6741b17b6f13ff365a29101b861b22b99c4db
Matthias Schmidt <mail@wolax.de>
2010-05-03 20:38:09 +0200

    adds delayed_job to plugins and adds migration

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100429144733) do
+ActiveRecord::Schema.define(:version => 20100503183618) do
 
   create_table "databases", :force => true do |t|
     t.integer  "user_id"
@@ -17,6 +17,21 @@ ActiveRecord::Schema.define(:version => 20100429144733) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.string   "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["locked_by"], :name => "index_delayed_jobs_on_locked_by"
 
   create_table "domains", :force => true do |t|
     t.integer  "user_id"
