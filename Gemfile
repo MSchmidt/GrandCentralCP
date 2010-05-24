c3500bf89c730aa93318e52b2059a913481973c2
Bettina Steger <bettina_steger@gmx.at>
2010-05-24 16:05:16 +0200

    bundler config

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source "http://rubygems.org"
+gem "rails", '2.3.5'
+gem "sqlite3-ruby", :require => "sqlite3"
+gem "haml", ">=2.2.24"
+gem "devise", "1.0.7", :require => "warden"
+gem "delayed_job", "2.0.3"
+
+group :test do
+  gem 'machinist'
+  gem 'faker'
+  gem 'webrat'
+  gem 'shoulda'
+  gem 'redgreen'
+end
+
--- config/boot.rb
+++ config/boot.rb
@@ -36,7 +36,13 @@ module Rails
   class Boot
     def run
       load_initializer
-      Rails::Initializer.run(:set_load_path)
+      Rails::Initializer.class_eval do
+      def load_gems
+        @bundler_loaded ||= Bundler.require :default, Rails.env
+      end
+    end
+
+    Rails::Initializer.run(:set_load_path)
     end
   end
 
