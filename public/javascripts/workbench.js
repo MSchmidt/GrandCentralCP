e0088c0ee6ba562f94641f36cfe244fe0dcca93c
Matthias Schmidt <mail@wolax.de>
2010-05-28 19:28:56 +0200

    styles domain workbench and improves drag

--- public/javascripts/workbench.js
+++ public/javascripts/workbench.js
@@ -1,22 +1,21 @@
 window.addEvent('domready', function(){
 	$$('.domain').each(function(i){
 		var myDrag = new Drag.Move(i, {
+			handle: i.getElement('.domain-nob'),
+			container: 'workbench',
 
-		    container: 'workbench',
+			onDrop: function(element, droppable, event){
+				if (!droppable) console.log(element, ' dropped on nothing');
+				else console.log(element, 'dropped on', droppable, 'event', event);
+			},
 
-		    onDrop: function(element, droppable, event){
-		        if (!droppable) console.log(element, ' dropped on nothing');
-		        else console.log(element, 'dropped on', droppable, 'event', event);
-		    },
+			onEnter: function(element, droppable){
+				console.log(element, 'entered', droppable);
+			},
 
-		    onEnter: function(element, droppable){
-		        console.log(element, 'entered', droppable);
-		    },
-
-		    onLeave: function(element, droppable){
-		        console.log(element, 'left', droppable);
-		    }
-
+			onLeave: function(element, droppable){
+				console.log(element, 'left', droppable);
+			}
 		});
 	});
 });
