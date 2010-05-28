4fa0ce18e0a234b38a487ac01ec36081aad83f4b
Matthias Schmidt <mail@wolax.de>
2010-05-28 02:14:55 +0200

    adds dragndrop support to workbench

--- public/javascripts/workbench.js
+++ public/javascripts/workbench.js
@@ -1,24 +1,22 @@
 window.addEvent('domready', function(){
-	$$('.domain').each(function(e){
-		console.log('whoot' + e);
+	$$('.domain').each(function(i){
+		var myDrag = new Drag.Move(i, {
+
+		    container: 'workbench',
+
+		    onDrop: function(element, droppable, event){
+		        if (!droppable) console.log(element, ' dropped on nothing');
+		        else console.log(element, 'dropped on', droppable, 'event', event);
+		    },
+
+		    onEnter: function(element, droppable){
+		        console.log(element, 'entered', droppable);
+		    },
+
+		    onLeave: function(element, droppable){
+		        console.log(element, 'left', droppable);
+		    }
+
+		});
 	});
-	
-	// var myDrag = new Drag.Move('draggable', {
-	// 
-	//     container: 'workbench',
-	// 
-	//     onDrop: function(element, droppable, event){
-	//         if (!droppable) console.log(element, ' dropped on nothing');
-	//         else console.log(element, 'dropped on', droppable, 'event', event);
-	//     },
-	// 
-	//     onEnter: function(element, droppable){
-	//         console.log(element, 'entered', droppable);
-	//     },
-	// 
-	//     onLeave: function(element, droppable){
-	//         console.log(element, 'left', droppable);
-	//     }
-	// 
-	// });
 });
